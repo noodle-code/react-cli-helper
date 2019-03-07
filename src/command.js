887daf5fab8f677a29d916eea0b68925e34a8c07
atomlab <aldrinthomaslab@gmail.com>
2019-03-07 18:41:20 +0800

    Fixed error detection conditionals

--- src/command.js
+++ src/command.js
@@ -6,10 +6,12 @@ const runNextCommand = callback => {
   let {commands, ...options} = commandArguments;
 
   if (commands.length <= 0) {
+    console.log('No more commands to run.');
     return false;
   }
 
-  if (!callback) {
+  if (!callback || typeof callback !== 'function') {
+    console.log('Invalid callback argument.');
     return false;
   }
 
