f83183cb44c2aa1be35be7ce253dcf820caaca8c
atomlab <aldrinthomaslab@gmail.com>
2019-03-08 15:48:01 +0800

    Implemented simpleparser

--- src/utilities/filesystem.js
+++ src/utilities/filesystem.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const template = require('./simpleparser');
 
 const fileNotExists = filename => new Promise((resolve, reject) => {
   if(fs.existsSync(filename)) {
@@ -15,7 +16,9 @@ const fileNotExists = filename => new Promise((resolve, reject) => {
 
 const _getWriteData = (option, encoding = 'utf8') => {
   if(option.template) {
-    return fs.readFileSync(option.template, encoding);
+    let string = fs.readFileSync(option.template, encoding);
+
+    return template.generateFromTemplate(string, option.data);
   }
 
   return (option.data) ? JSON.stringify(option.data) : '';
